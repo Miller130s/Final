3f286344d8d1cd1ee67eebe68a923379f0bb033d
Miller130s <nmiller1269@gmail.com>
2026-04-10 10:15:37 -0700

    n

--- pages/6_Recommendations.py
+++ pages/6_Recommendations.py
@@ -14,10 +14,15 @@ st.markdown(
 - More people to support increase of missions
 
  - Integrating the need for heavy-lift infrastructure
- 
+
 - Maximize MAT Capacity and Readiness via Predictive Manpower Modeling
 
 """
 )
 
 st.video("https://youtu.be/Ewvs3fxxj9M?si=g1DG0YuEaKqQTfYu", autoplay=False)
+
+# st.video("https://youtu.be/AcoFvwsuIQI?si=XPQWlL38kYFbspoGstart=360&end=377"", autoplay=False)
+         
+# Use a '?' to start parameters and an '&' to separate them.
+st.video("https://www.youtube.com/watch?v=AcoFvwsuIQI&start=360&end=377", autoplay=False)
