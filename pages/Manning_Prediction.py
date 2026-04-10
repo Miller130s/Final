591d8f8d11b059093e2e80f07aabf06ea9ed2656
Miller130s <nmiller1269@gmail.com>
2026-04-10 09:04:00 -0700

    n

--- pages/Manning_Prediction.py
+++ pages/Manning_Prediction.py
@@ -8,19 +8,19 @@ import math
 
 
 missions = st.slider("Select the number of missions happening at one time", min_value=1, max_value=10)
-tasks_per_mission = st.slider("Select the average number of important tasks per mission(Average 30 A-C Observes)", min_value=1, max_value=500)
+tasks_per_mission = st.slider("Select the average number of observers per mission (Average 30 A-C Observes)", min_value=1, max_value=500, value=30)
 total_tasks = missions * tasks_per_mission
-shift = st.slider("Select the shift", min_value=1, max_value=3)
-locations = st.slider("Select the number of locations", min_value=1, max_value=3)
-campaign_in_weeks = st.slider("Select the campaign duration in weeks", min_value=1, max_value=52)
+# shift = st.slider("Select the shift", min_value=1, max_value=3)
+# locations = st.slider("Select the number of locations", min_value=1, max_value=3)
+campaign_in_weeks = st.slider("Select the campaign duration in weeks", min_value=1, max_value=52, value=4)
 
 # st.dataframe(pd.read_csv("data/man.csv"))
 power_df = pd.DataFrame({
     "missions": [missions],
     "tasks_per_mission": [tasks_per_mission],
     "total_tasks": [total_tasks],
-    "shift": [shift],
-    "location": [locations],
+    # "shift": [shift],
+    # "location": [locations],
     "campaign_in_weeks": [campaign_in_weeks]
 })
 
@@ -36,10 +36,10 @@ values = [math.floor(total * w) for w in weights]
 # Fix rounding on last value
 values[-1] = total - sum(values[:-1])
 
-st.write("ASO:", values[0])
-st.write("SLC4:", values[1])
-st.write("B398:", values[2])
-st.write("Total:", total)
+# st.write("ASO:", values[0])
+# st.write("SLC4:", values[1])
+# st.write("B398:", values[2])
+st.write("Total MATS Needed:", total)
 # ASO = st.write("Predicted MATS at ASO:", math.ceil(manpower_prediction[0] * 0.2))
 # SLC4 = st.write("Predicted MATS at SLC4:", math.ceil(manpower_prediction[0] * 0.4))
 # B398 = st.write("Predicted MATS at B398:", math.ceil(manpower_prediction[0] * 0.4))
