73bc3e9da9ad7e4a9f4b58e8e1b51782d018f9dc
Jake Bergen <jakerbergen@gmail.com>
2026-04-10 07:55:06 -0700

    m

--- pages/Future Trends.py
+++ pages/Future Trends.py
@@ -12,9 +12,9 @@ st.title("Future Trends in Launch Volume and Payload Weight")
 st.image("Vandenberg_Prediction.png")
 st.write("The graph above shows the predicted number of launches from Vandenberg Space Force Base from 2026 to 2030. The prediction is based on historical data and trends in the space industry. The graph indicates a steady increase in the number of launches, suggesting a growing demand for space access and exploration in the coming years.")
 
-st.image("Cape Prediction (2026–2030).png")
+st.image("Cape_Prediction.png")
 st.write("The graph above shows the predicted number of launches from Cape Canaveral Space Force Station from 2026 to 2030. The prediction is based on the recent growth in launches from this site.")
 
-st.image("SpaceX Payload Weight Road to 2030.png")
+st.image("SpaceX_Payload_Weight_Road_o_2030.png")
 st.write("According to trend analysis we predict a growth in the number of heavy payload launches as well as a increase in payload weight.")
 
--- pages/Manning_Prediction.py
+++ pages/Manning_Prediction.py
@@ -7,20 +7,29 @@ import math
 
 
 
-missions = st.slider("Select the number of missions", min_value=1, max_value=10)
-tasks_per_mission = st.slider("Select the average number of important tasks per mission", min_value=1, max_value=500)
+missions = st.slider("Select the number of missions happening at one time", min_value=1, max_value=10)
+tasks_per_mission = st.slider("Select the average number of important tasks per mission(Average 30 A-C Observes)", min_value=1, max_value=500)
 total_tasks = missions * tasks_per_mission
+shift = st.slider("Select the shift", min_value=1, max_value=3)
+locations = st.slider("Select the number of locations", min_value=1, max_value=3)
+campaign_in_weeks = st.slider("Select the campaign duration in weeks", min_value=1, max_value=52)
+
 # st.dataframe(pd.read_csv("data/man.csv"))
 power_df = pd.DataFrame({
     "missions": [missions],
     "tasks_per_mission": [tasks_per_mission],
-    "total_tasks": [total_tasks]
-    
+    "total_tasks": [total_tasks],
+    "shift": [shift],
+    "location": [locations],
+    "campaign_in_weeks": [campaign_in_weeks]
 })
 
-data = joblib.load('manpower_predict.joblib')
+data = joblib.load('PB_predict.joblib')
 
 model = data["model"]
 manpower_prediction = model.predict(power_df)
 
-st.write("Predicted Manpower:", math.ceil(manpower_prediction[0]))
+st.write("Total Predicted Manpower(MATS):", math.ceil(manpower_prediction[0]))
+st.write("Predicted MATS at ASO:", math.ceil(manpower_prediction[0] * 0.2))
+st.write("Predicted MATS at SLC4:", math.ceil(manpower_prediction[0] * 0.4))
+st.write("Predicted MATS at B398:", math.ceil(manpower_prediction[0] * 0.4))
