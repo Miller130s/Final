1201b082d1e18722df2f1e6d8db85651f25de9d5
Jake Bergen <jakerbergen@gmail.com>
2026-04-16 08:36:31 -0700

    m

--- pages/7_How To Contact Us.py
+++ pages/7_How To Contact Us.py
@@ -4,7 +4,9 @@ import streamlit as st
 st.write("How to Contact Us")
 
 st.write("""
-If you have any questions, suggestions, or would like to get in touch with us, please feel free to reach out through the following channels: 
+If you have any questions, suggestions, or would like to get in touch with us, please feel free to reach out through the following channels:
+
+         
 -Jake Bergen
 - Email: jakerbergen@gmail.com
 - Phone: +1 (402) 850-5755
